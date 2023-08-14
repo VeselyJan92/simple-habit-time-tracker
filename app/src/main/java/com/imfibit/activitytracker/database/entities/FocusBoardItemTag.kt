22fc77c0be6b7f625c16d3f9894d705a215628e1
Jan Veselý <jan.vesely92@gmail.com>
2023-08-14 19:54:29 +0200

    Update Focus Tag colors

--- app/src/main/java/com/imfibit/activitytracker/database/entities/FocusBoardItemTag.kt
+++ app/src/main/java/com/imfibit/activitytracker/database/entities/FocusBoardItemTag.kt
@@ -30,17 +30,31 @@ data class FocusBoardItemTag(
         const val TABLE = "focus_board_item_tags"
 
         val colors = listOf(
+            Color(0xFFFFCDD2),
+            Color(0xFFF8BBD0),
             Color(0xFFE1BEE7),
+
             Color(0xFFD1C4E9),
             Color(0xFFC5CAE9),
             Color(0xFFBBDEFB),
+
             Color(0xFFB3E5FC),
+            Color(0xFFB2EBF2),
             Color(0xFFB2DFDB),
+
             Color(0xFFC8E6C9),
             Color(0xFFDCEDC8),
             Color(0xFFF0F4C3),
+
             Color(0xFFFFF9C4),
             Color(0xFFFFECB3),
+            Color(0xFFFFE0B2),
+
+            Color(0xFFFFCCBC),
+            Color(0xFFD7CCC8),
+            Color(0xFFF5F5F5),
+
+            Color(0xFFCFD8DC),
         )
     }
 
