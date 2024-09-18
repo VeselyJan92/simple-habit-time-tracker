625cc243f8dec47ef98d643d59349ca55707154b
Jan Veselý <jan.vesely92@gmail.com>
2024-09-18 20:36:21 +0200

    Fix export not uptodate

--- app/src/main/java/com/imfibit/activitytracker/ui/screens/settings/ScreenSettingVM.kt
+++ app/src/main/java/com/imfibit/activitytracker/ui/screens/settings/ScreenSettingVM.kt
@@ -3,18 +3,28 @@ package com.imfibit.activitytracker.ui.screens.settings
 import android.app.Activity
 import android.content.Context
 import android.net.Uri
-import androidx.lifecycle.ViewModel
+import androidx.room.withTransaction
+import com.imfibit.activitytracker.core.BaseViewModel
 import com.imfibit.activitytracker.database.AppDatabase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlin.system.exitProcess
 
 @HiltViewModel
-class ScreenSettingVM @Inject constructor() : ViewModel(){
+class ScreenSettingVM @Inject constructor(
+    val db: AppDatabase
+) : BaseViewModel(){
 
-    fun exportDB(context: Context, uri: Uri){
+    fun exportDB(context: Context, uri: Uri) = launchIO {
+        db.query("PRAGMA wal_checkpoint(FULL)", null).moveToFirst()
+        db.query("VACUUM", null).moveToFirst()
+
         val bytes = context.getDatabasePath(AppDatabase.DB_NAME).readBytes()
-        context.contentResolver.openOutputStream(uri)?.write(bytes)
+
+        context.contentResolver.openOutputStream(uri)?.apply {
+            write(bytes)
+            close()
+        }
     }
 
     fun importDB(context: Context, uri: Uri){
