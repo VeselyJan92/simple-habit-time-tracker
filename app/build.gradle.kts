9ab2fefff5147b3403d9880b228cddbd8886fb67
Jan Veselý <jan.vesely92@gmail.com>
2024-12-08 10:04:35 +0100

    RELEASE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
         applicationId = "com.imfibit.activitytracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
+        versionCode = 41
         versionName = "1.4.1"
         testInstrumentationRunner  ="com.imfibit.activitytracker.HiltRunner"
     }
